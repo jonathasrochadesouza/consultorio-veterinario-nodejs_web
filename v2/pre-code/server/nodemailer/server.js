8c800719f02896c1d244712a88820ee72100dc7c
Jonathas Rocha de Souza <62067474+jonathasrochadesouza@users.noreply.github.com>
2021-07-03 11:24:24 -0300

    Update 'example password'

--- v2/pre-code/server/nodemailer/server.js
+++ v2/pre-code/server/nodemailer/server.js
@@ -1,25 +1,31 @@
+require('dotenv').config();
+
 const nodemailer = require('nodemailer');
 
 //Step 1
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: '',
-        pass: ''
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
     }
 });
 
 //Step 2
 let mailOptions = {
-    from: '',
-    to: '',
-    subject: '',
-    text: ''
-}
+    from: 'jonathasrochad@gmail.com',
+    to: 'g2016arocha@gmail.com',
+    subject: 'Sua senha de acesso (Consultorio veterinario)',
+    text: 'Access consultorio veterinario'
+};
 
 //Step 3
 transporter.sendMail(mailOptions, function(err, data) {
-
+    if (err) {
+        console.log('Error Occors: ', err);
+    } else {
+        console.log('Email send!');
+    }
 });
 
 
